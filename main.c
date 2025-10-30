d67d612e9ab7a37e8cf381ae6647238b4aad844b
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-30 16:05:23 +0900

    실습 3 -2 (원래 목표)
    다시 해야돼

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 
 #define SIZE 5
-//3. 배열 값 비교 
+//3. 배열 값 비교 (원래 목표) 
 int main(void)
 {
     //SIZE 크기 배열 선언 
   int i; 
   int a[SIZE] = {1,2,3,4,5};
   int b[SIZE] = {1,2,3,4,5};
+  int flag_same = 1;
   
-  if(a==b) //올바르지 않은 배열 비교
-  printf("The same.\n");
-  else
-   printf("Not same.\n");
+  for (i=0;i<SIZE;i++)
+  {
+      if (a[i] != b[i])
+      printf("%i-th element is diff.\n", i);
+      flag_same=0;
+      } 
+      
+  printf("Are a and b the same? %i\n", flag_same);
   
   system("PAUSE");	
   return 0;
