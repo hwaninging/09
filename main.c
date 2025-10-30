59cbe18ee27d4371ad53ae55ae0f7742fbce3d08
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-30 15:39:11 +0900

    실습 2: 배열 및 반복문 활용
    5명 점수 입력하면
    grade로 다 나옴

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-//1. 배열의 활용 
-//5명의 학생에 대한 시험성적 배열 선언 
+
+#define SIZE 5 //a를 b로 바꾼다 : SIZE로 바꿔준다. 
+//2. 배열 및 반복문 활용 (고급 버전) 
 int main(void)
 {
   int i; 
-  int test_score[5]; ////시험 성적 : 배열 이름  배열 크기 : 5명 
+  int test_score[SIZE]; //다른 사람이 봐도 쉽게 이해할 수 있게 표현. 
+  printf("i명의 점수를 입력하세요. \n", SIZE);  //점수 입력 
   
-  //점수 넣어주는 코드  
-  test_score[0] = 10;
-  test_score[1] = 20;
-  test_score[2] = 30;
-  test_score[3] = 40;
-  test_score[4] = 50;
+  //점수 화면 출력 
+  for(i=0;i<SIZE;i++)
+  scanf("%d", &test_score[i]);
   
-  //점수 화면 출력 (반복문 활용) 
-  for(i=0;i<5;i++)
-  printf("grade[%i] = %i\n", i, test_score[i]); 
+  for(i=0;i<SIZE;i++)
+  printf("test_score[%d] = %d\n", i, test_score[i]);
   
   system("PAUSE");	
   return 0;
