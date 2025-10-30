234f7ec054cd3e80a381cecc3b5d57eb369fb5ea
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-30 15:57:17 +0900

    실습 3-1

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5 //a를 b로 바꾼다 : SIZE로 바꿔준다. 
-//2. 배열 및 반복문 활용
-//학생 성적 평균 (간단 ver.) 
+#define SIZE 5
+//3. 배열 값 복사 
 int main(void)
 {
+    //SIZE 크기 배열 선언 
   int i; 
-  int test_score[SIZE]; //다른 사람이 봐도 쉽게 이해할 수 있게 표현. 
-  int sum = 0; //누적으로 연산할 때 초기화 필수. 
-  printf("i명의 점수를 입력하세요. \n", SIZE);  //점수 입력 
-  
+  int grade[SIZE];
+  int score[SIZE];
   
   for(i=0;i<SIZE;i++)
   {
-                     scanf("%d", &test_score[i]);
-                     sum += test_score[i];
+      grade[i] = rand()%100; //0~99사이의 값이 랜덤하게 나오게 함. 
 }
+ for(i=0;i<SIZE;i++)
+  {
+      score[i] = grade[i]; 
+}
+
  //점수 화면 출력
-  #if 0 //전처리기 - #안에 있는 애들이 지워진다. (=지우긴 아깝고 컴파일은 시러) 
+  #if 1 
   for(i=0;i<SIZE;i++)
-  printf("test_score[%i] = %i\n", i, test_score[i]);
-  #endif //막아주었다.
-  printf("성적 평균: %i\n"); 
+      printf("grade[%i] = %i, score[%i] = %i\n", i, grade[i], i, score[i]);
+  #endif
   
   system("PAUSE");	
   return 0;
