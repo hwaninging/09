e05ef4b7ed5fd5b7d860227282a30ebc7883846e
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-30 16:00:23 +0900

    실습 3-2
    Not same.

--- main.c
+++ main.c
@@ -2,28 +2,18 @@
 #include <stdlib.h>
 
 #define SIZE 5
-//3. 배열 값 복사 
+//3. 배열 값 비교 
 int main(void)
 {
     //SIZE 크기 배열 선언 
   int i; 
-  int grade[SIZE];
-  int score[SIZE];
+  int a[SIZE] = {1,2,3,4,5};
+  int b[SIZE] = {1,2,3,4,5};
   
-  for(i=0;i<SIZE;i++)
-  {
-      grade[i] = rand()%100; //0~99사이의 값이 랜덤하게 나오게 함. 
-}
- for(i=0;i<SIZE;i++)
-  {
-      score[i] = grade[i]; 
-}
-
- //점수 화면 출력
-  #if 1 
-  for(i=0;i<SIZE;i++)
-      printf("grade[%i] = %i, score[%i] = %i\n", i, grade[i], i, score[i]);
-  #endif
+  if(a==b) //올바르지 않은 배열 비교
+  printf("The same.\n");
+  else
+   printf("Not same.\n");
   
   system("PAUSE");	
   return 0;
